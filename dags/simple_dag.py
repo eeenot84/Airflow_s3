ce42753f9360e5ef3e412ffeca0991e83bdd5fb4
Rinat <eenot84@yandex.ru>
2025-08-02 23:34:10 +0400

    yth

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,9 +1,9 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime, timedelta
-
-def hello_airflow():
-    print("Hello, Airflow!")
+import requests
+import json
 
 default_args = {
     'owner': 'airflow',
@@ -13,15 +13,48 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
+S3_CONN_ID = 'my_s3_conn'
+BUCKET_NAME = 'my-bucket'
+OBJECT_KEY = 'data/api_data.json'
+
+def fetch_api_data(**context):
+    url = 'https://jsonplaceholder.typicode.com/todos/1'
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    context['ti'].xcom_push(key='api_data', value=data)
+
+def upload_to_s3(**context):
+    data = context['ti'].xcom_pull(key='api_data', task_ids='fetch_api_data')
+    json_bytes = json.dumps(data).encode('utf-8')
+
+    s3_hook = S3Hook(aws_conn_id=S3_CONN_ID)
+    s3_hook.load_bytes(
+        bytes_data=json_bytes,
+        key=OBJECT_KEY,
+        bucket_name=BUCKET_NAME,
+        replace=True,
+    )
+    print(f"Файл загружен в S3: s3://{BUCKET_NAME}/{OBJECT_KEY}")
+
 with DAG(
-    'simple_hello_dag',
+    'api_to_s3_with_hook',
     default_args=default_args,
-    description='Простой DAG с выводом в лог',
-    schedule_interval='*/1 * * * *',  # каждую минуту
+    description='DAG тянет данные из API и загружает в S3 через S3Hook',
+    schedule_interval='@hourly',
     catchup=False,
 ) as dag:
 
-    task_hello = PythonOperator(
-        task_id='say_hello',
-        python_callable=hello_airflow
+    t1 = PythonOperator(
+        task_id='fetch_api_data',
+        python_callable=fetch_api_data,
+        provide_context=True,
     )
+
+    t2 = PythonOperator(
+        task_id='upload_to_s3',
+        python_callable=upload_to_s3,
+        provide_context=True,
+    )
+
+    t1 >> t2
